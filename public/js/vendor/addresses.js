beb96f8da52aea6ee419e380127d5943a4dce694
Andrew Smith <andrewchristophersmith@gmail.com>
2013-08-09 18:15:30 -0700

    Added email obfuscator

---
+++ public/js/vendor/addresses.js
@@ -0,0 +1,16 @@
+function setAddr(name) {
+	var obj = document.getElementById('contactAddr-' + name);
+	if (obj) {
+		switch(name) {
+			case 'b': name += 'eau'; break;
+			case 'i': name += 'nfo'; break;
+      case 'a': name += 'ndrew'; break;
+		}
+		var dmn = new Array('org','.','al','ic','dex','in').reverse().toString();
+		dmn = dmn.replace(/\,/g,'').replace(/(w)/g,'$1eb');
+		obj.href = 'mailto:' + name + '@' + dmn;
+	}
+	return true;
+}
+
+// Email address obfuscator by Eric Meyer, http://meyerweb.com/
